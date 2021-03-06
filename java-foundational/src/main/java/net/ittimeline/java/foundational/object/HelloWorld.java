b43104d07ce85f71a9e5476d62821f2e49d584c2
ittimeline <ittimelinedotnet@gmail.com>
2021-03-06 18:45:07 +0800

    我的第一个Java程序

---
+++ java-foundational/src/main/java/net/ittimeline/java/foundational/object/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.ittimeline.java.foundational.object;
+
+/**
+ * 我的第一个Java程序
+ *
+ * @author tony 18601767221@163.com
+ * @version 2021/3/6 18:42
+ * @since JDK11
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World with Windows10 & JDK11 & Git & Maven &IntelliJ IDEA 2020.3.2");
+    }
+}
